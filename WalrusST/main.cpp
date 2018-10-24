89f55d22b2ad4c752b25e556432260179b0872f3
Maxim Grachev <mxmgrachev@gmail.com>
2018-10-24 16:01:53 +0300

    moveFiles and timeout functions were added

--- WalrusST/main.cpp
+++ WalrusST/main.cpp
@@ -1,72 +1,82 @@
-#include <QDir>
-#include <QtCore>
+#include <windows.h>
 #include <iostream>
+#include <stdlib.h>
+#include <string.h>
+#include <tchar.h>
+
 using namespace std;
 
-
-void showSettings(){
-    int point;
-    cout<<"1. Choose EumetSat receiving folder"<<endl;
-    cout<<"2. Choose workspace"<<endl;
-    cout<<"3. Set Timeout"<<endl;
-    cout<<"4. Close"<<endl;
-    cin>>point;
-
-    switch(point){
-        case 1: {
-            cout << "Choose EumetSat receiving folder"<<endl;
-            showSettings();
-            break;
-        }
-        case 2: {
-            cout << "Choose workspace"<<endl;
-            break;
-        }
-        case 3: {
-            cout << "Set Timeout"<<endl;
-            break;
-        }
-        default: {
-            cout << "Error!"<<endl;
-            break;
-        }
-    }
+void sortFiles(){
+    MoveFile(L"D:\\myProgramms\\C++\\Eumetsat_files\\1\\1.l2_bufr", L"D:\\myProgramms\\C++\\Eumetsat_files\\2\\1.txt");
 
 }
-
-
-void showMainMenu(){
-    int point;
-    cout<<"Menu"<<endl;
-    cout<<"1. Setting"<<endl;
-    cout<<"2. Run program"<<endl;
-    cout<<"3. Quit"<<endl;
-    cin>>point;
-
-    switch(point){
-        case 1: {
-            cout << "Setting"<<endl;
-            showSettings();
-            break;
-        }
-        case 2: {
-            cout << "Run program"<<endl;
-            break;
-        }
-        case 3: {
-            cout << "Quit. Press any key"<<endl;
-            break;
-        }
-        default: {
-            cout << "Error!"<<endl;
-            break;
-        }
-    }
-}
-
 
 int main(){
-    showMainMenu();
+    sortFiles();
 
-    return 0;
+    Sleep(10000); // timeout to 10 000 ms
+    cout<<"10s was going"<<endl;
+
 }
+
+
+//void showSettings(){
+//    int point;
+//    cout<<"1. Choose EumetSat receiving folder"<<endl;
+//    cout<<"2. Choose workspace"<<endl;
+//    cout<<"3. Set Timeout"<<endl;
+//    cout<<"4. Close"<<endl;
+//    cin>>point;
+
+//    switch(point){
+//        case 1: {
+//            cout << "Choose EumetSat receiving folder"<<endl;
+//            showSettings();
+//            break;
+//        }
+//        case 2: {
+//            cout << "Choose workspace"<<endl;
+//            break;
+//        }
+//        case 3: {
+//            cout << "Set Timeout"<<endl;
+//            break;
+//        }
+//        default: {
+//            cout << "Error!"<<endl;
+//            break;
+//        }
+//    }
+
+//}
+
+
+//void showMainMenu(){
+//    int point;
+//    cout<<"Menu"<<endl;
+//    cout<<"1. Setting"<<endl;
+//    cout<<"2. Run program"<<endl;
+//    cout<<"3. Quit"<<endl;
+//    cin>>point;
+
+//    switch(point){
+//        case 1: {
+//            cout << "Setting"<<endl;
+//            showSettings();
+//            break;
+//        }
+//        case 2: {
+//            cout << "Run program"<<endl;
+//            break;
+//        }
+//        case 3: {
+//            cout << "Quit. Press any key"<<endl;
+//            break;
+//        }
+//        default: {
+//            cout << "Error!"<<endl;
+//            break;
+//        }
+//    }
+//}
+
